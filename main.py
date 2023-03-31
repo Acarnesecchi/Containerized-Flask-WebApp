9eab0c0f1ea965c3d1ed8d438ca9f44ac20e045c
Acarnesecchi <alexandro.carnesecchi@estudiants.urv.cat>
2023-03-31 16:01:26 +0200

    Simple QR generation app. Works in local

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import qrcode
+import os
 
 app = Flask(__name__, template_folder='view', static_folder='static')
 
@@ -20,7 +21,9 @@ def generate_qr_code(text):
     qr.add_data(text)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
-    return img
+    filename = f"{text}.png"
+    img.save(os.path.join('static/qr/', filename))
+    return render_template('display.html', img=filename)
 
 
 if __name__ == '__main__':
